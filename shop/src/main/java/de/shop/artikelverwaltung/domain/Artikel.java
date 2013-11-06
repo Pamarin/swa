9f3827b1adbbd45e8e097ef0cfe5fb4f3d290220
hema <hema@Unilaptop.fritz.box>
2013-11-06 18:31:53 +0100

    Klasse Artikel noch ohne Methoden aber kommt ;)

--- shop/src/main/java/de/shop/artikelverwaltung/domain/Artikel.java
+++ shop/src/main/java/de/shop/artikelverwaltung/domain/Artikel.java
@@ -10,7 +10,7 @@ import java.math.BigDecimal;
 
 public class Artikel {
 	// geschrieben von Marvin
-	// ToDo Set Preis!
+	// ToDo Beans Validation für Verfügbarkeit
 	
 	enum Verfügbarkeit{
 		istVerfügbar,
