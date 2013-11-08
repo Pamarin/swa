f9777bbd1f334507de94cbf41a777d7f265563b5
Jean <Jean@lpt-jean-lat>
2013-11-08 15:11:21 +0100

    JaxRsActivator wieder hinzugefügt.

---
+++ shop/src/main/java/de/shop/util/JaxRsActivator.java
@@ -0,0 +1,11 @@
+package de.shop.util;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/**
+ * @author Jean-Luc Burot
+ */
+@ApplicationPath("/rest")
+public class JaxRsActivator extends Application {
+}
